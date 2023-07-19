414f7f64c4ab841791021af62c778c965189d1db
maxim_mikheev <presswallperm@yandex.ru>
2023-07-19 09:39:53 +0300

    new structure and refactoring

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+import { useTheme } from "app/providers/ThemeProvider";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppRouter } from "./providers/router";
+import "./styles/index.scss"
+
+const App = () => {
+  const {theme, toggleTheme} = useTheme();
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <AppRouter />
+    </div>
+  )
+};
+
+export default App;
